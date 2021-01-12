83c58b176afc3f61acc75d445184279ecd32cd2c
Nguyen Van Au <34223733+nguyenvanauVietNam@users.noreply.github.com>
2021-01-12 08:42:39 +0700

    Update

---
+++ Learning/CPP/C/C_for_beginer/hello.c
@@ -0,0 +1,14 @@
+//1. Tiền xử lý ( header)
+#include <stdio.h>
+
+//2 Hàm main
+int main (void)
+{
+    //3 Biến và hàm
+    printf("Hello Word!");
+}
+//4 Đây là comments
+
+/*
+Mỗi chương trình c cơ bản gồm 4 phần
+*/
